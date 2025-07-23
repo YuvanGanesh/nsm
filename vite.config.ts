323c7945273c8040bc0f7afe75868c2f5765baca
YuvanGanesh <yuvanganesh276@gmail.com>
2025-07-23 19:39:36 +0530

    commit

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/project1/',
+  base: '/nsm/', // <-- important!
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
